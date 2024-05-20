a28cd19b8844642f168a34b4df862fb239645402
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2024-05-20 16:54:45 +0200

    Output version information in logging

--- src/collector_node/collector_node.py
+++ src/collector_node/collector_node.py
@@ -280,6 +280,7 @@ async def main():
 
     # Stagger the collection of the data in this script so that the
     # validator node isn't flooded each round.
+    logging.info("collector-node version: '%s'", get_version())
     await asyncio.sleep(random.randint(1, 20))
     identity = await read_identity()
     await fetch_and_send(identity)
